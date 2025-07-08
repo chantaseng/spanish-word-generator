69761b2f2ca4faff53bd55f26c2299823312c308
William Chan <chan.william.ts@gmail.com>
2025-07-08 16:19:33 +0200

    styling learn page

--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -1,7 +1,41 @@
+import { Menu } from 'lucide-react';
+
 function Learn() {
   return (
     <>
-      <div>Learn page</div>
+      <div className="navbar">
+        <h1>Palabrify</h1>
+        <Menu size={48} />
+      </div>
+      <div className="container">
+        <div className="learn-page">
+          <div className="controls-container">
+            <div className="game-container">
+              <label htmlFor="gameSelect">Game:</label>
+              <select id="gameSelect">
+                <option value="learning">Learning</option>
+                <option value="proverb">Proverb</option>
+              </select>
+            </div>
+
+            <div className="category-container">
+              <label htmlFor="categorySelect">Category:</label>
+              <select id="categorySelect">
+                <option value="easy">Easy</option>
+                <option value="moderate">Moderate</option>
+                <option value="hard">Hard</option>
+                <select />
+              </select>
+            </div>
+
+            <button className="button-container">New Word</button>
+          </div>
+
+          <div className="result-container">
+            <h1>Palabra</h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
